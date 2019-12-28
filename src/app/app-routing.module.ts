885daa5fd707b34bf9a906f75a520d25dadbb3dd
nikhilkharbas <nikhilkharbas1208@gmail.com>
2019-12-28 23:10:13 +0530

    rendor the login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
-    {
-        path: '',
-        loadChildren: './layout/layout.module#LayoutModule',
-        canActivate: [AuthGuard]
-    },
-    {
+        {
         path: 'login',
         loadChildren: './login/login.module#LoginModule'
     }
