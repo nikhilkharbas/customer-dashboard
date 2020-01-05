9eef0c906a471085be340825afbd53935caee7f7
abhijeet <abhijeet.r.more@gmail.com>
2020-01-05 13:00:40 +0530

    New TS

--- server/config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'connection':{
-     'host':'localhost',
-     'user':'root',
-     'password':''
-    },
-    'database':'management_console',
-    'user_table':'users'
-   }
